8a95aefa296455401fd8535142a008d92ba12020
daejlee <savif4991@gmail.com>
2023-07-30 02:10:34 +0900

    Rename: Udata.hpp추가

---
+++ kqueueServer/UData.hpp
@@ -0,0 +1,30 @@
+#ifndef UDATA_HPP
+# define UDATA_HPP
+# include <iostream>
+# include <string>
+# include <vector>
+# include "../interface/HttpRequest.hpp"
+# include "../interface/HttpResponse.hpp"
+
+typedef enum errorType{
+	OK = 0,
+}e_error;
+
+class UData{
+	public :
+		/* constructor */
+		UData();
+
+		/* variables */
+		HttpRequest				http_request_;
+		HttpResponse			http_response_;
+		e_error 					error_flag_;
+		std::vector<char>	raw_data_;
+
+		/* cgi */
+		std::string				prog_name_;
+		std::vector<char>	cgi_store_;
+	private :
+};
+
+#endif

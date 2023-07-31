ee96e7cc17e5b8cad23199b5dcf9871ed50d669c
daejlee <savif4991@gmail.com>
2023-07-31 17:46:03 +0900

    Feat: fd_type_ enum 추가와 그에 따른 fd_type_멤버변수 추가 / 생성자, 소멸자 추가 / 컴파일을 위한 Http관련 멤버들 일시적 주석처리

--- kqueueServer/UData.hpp
+++ kqueueServer/UData.hpp
@@ -3,21 +3,29 @@
 # include <iostream>
 # include <string>
 # include <vector>
-# include "../interface/HttpRequest.hpp"
-# include "../interface/HttpResponse.hpp"
+// # include "../interface/HttpRequest.hpp"
+// # include "../interface/HttpResponse.hpp"
 
 typedef enum errorType{
 	OK = 0,
 }e_error;
 
+typedef enum fdType{
+	SERVER,
+	CLNT,
+	CGI,
+}e_fd_type;
+
 class UData{
 	public :
 		/* constructor */
-		UData();
+		UData(e_fd_type type);
+		~UData();
 
 		/* variables */
-		HttpRequest				http_request_;
-		HttpResponse			http_response_;
+		e_fd_type					fd_type_;
+		// HttpRequest				http_request_;
+		// HttpResponse			http_response_;
 		e_error 					error_flag_;
 		std::vector<char>	raw_data_;
 

bbdf653b65105c3bef9a660f0c4a360a6ac6c47e
wnddms12345 <wnddms12345@naver.com>
2023-08-04 20:23:51 +0900

    Feat: Add Conf file wild Card - 이제 conf파일에서 . 으로 끝나는 wild카드를 받아서 사용합니다.

--- modules/webserv/WebServ.cpp
+++ modules/webserv/WebServ.cpp
@@ -3,6 +3,8 @@
 void	WebServ::startServer(int argc, char** argv){
 	try{
 		ConfParser::getInstance().configParseAll(argc, argv);
+		// ServBlock s= ConfParser::getInstance().getServBlock(8080,"localhost");
+		// s.findLocBlock("index.shsh").printInfo();
 		ServManager::getInstance().serverInit();
 		ServManager::getInstance().launchServer();
 	}catch(std::exception &e){

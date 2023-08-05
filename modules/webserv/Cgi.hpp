fec2ba6adbb8f67c07244c62f35a385ea470ba83
daejlee <savif4991@gmail.com>
2023-08-05 16:30:53 +0900

    Style: CGI Request form 레퍼런스를 위해 추가됨

--- modules/webserv/Cgi.hpp
+++ modules/webserv/Cgi.hpp
@@ -7,6 +7,15 @@
 # include <unistd.h>
 # include <fcntl.h>
 
+// - CGI Request form -
+// POST /cgi-bin/simple_cgi.cgi HTTP/1.1
+// Host: www.example.com
+// User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0
+// Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
+// Content-Type: application/x-www-form-urlencoded
+// Content-Length: 23
+// name=John&age=30
+
 /**
  * @brief CGI 프로세스를 생성하는 기능을 제공하는 정적 클래스입니다.
  */

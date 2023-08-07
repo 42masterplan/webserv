85d63eb9561c4f506ef8a6dd3c50aab04f5848cf
besyia0k0 <besyia0k0@gmail.com>
2023-08-07 16:38:25 +0900

    Fix: http request 여러 개 연달아 parsing

--- modules/webserv/ServManager.cpp
+++ modules/webserv/ServManager.cpp
@@ -155,7 +155,6 @@ void  ServManager::registerNewClnt(int serv_sockfd){
  */
 void  ServManager::sockReadable(struct kevent *cur_event){
 	UData*	cur_udata = (UData*)cur_event->udata;
-
 	if (cur_event->flags == EV_EOF){
 		disconnectFd(cur_event);
 		return;
@@ -178,16 +177,13 @@ void  ServManager::sockReadable(struct kevent *cur_event){
 		buff_[rlen] = '\0';
 		raw_data_ref.insert(raw_data_ref.end(), buff_, buff_ + rlen);
 
-		std::vector<HttpRequest> http_request_ref = cur_udata->http_request_;
-		if (cur_udata->http_request_.size() && \
-			(cur_udata->http_request_.back().getParseStatus() != FINISH && !cur_udata->http_request_.back().getRequestError()))
-			cur_udata->http_request_.back().parse(raw_data_ref);
-		while (cur_udata->http_request_.size() == 0 || \
-			(cur_udata->http_request_.back().getParseStatus() == FINISH && !cur_udata->http_request_.back().getRequestError())) {
-			cur_udata->http_request_.push_back(HttpRequest());
-			cur_udata->http_request_.back().parse(raw_data_ref);
+		std::vector<HttpRequest>& http_request_ref = cur_udata->http_request_;
+		if (http_request_ref.size() && (http_request_ref.back().getParseStatus() != FINISH && !http_request_ref.back().getRequestError()))
+			http_request_ref.back().parse(raw_data_ref);
+		while (http_request_ref.size() == 0 || (http_request_ref.back().getParseStatus() == FINISH && !http_request_ref.back().getRequestError())) {
+			http_request_ref.push_back(HttpRequest());
+			http_request_ref.back().parse(raw_data_ref);
 		}
-		
 		// if (cur_udata->http_request_.size() == 0 || \
 		// 	(cur_udata->http_request_.back().getParseStatus() == FINISH && cur_udata->raw_data_.size())){
 		// 	HttpRequest request_parser;
@@ -196,7 +192,7 @@ void  ServManager::sockReadable(struct kevent *cur_event){
 		// cur_udata->http_request_.back().parse(raw_data_ref);
 		// cur_udata->http_request_.back().printBodyInfo();
 
-		if (cur_udata->http_request_.back().getRequestError()) {
+		if (http_request_ref.back().getRequestError()) {
 			//아에 잘못 된 형식으로 메세지가 온 경우들에 대해서 Request 단에서 에러를 처리해줍니다.
 			//클라이언트 소켓 read_event 삭제 -> 파일 read_event 등록 -> 파일 read가 끝나면 그 파일을 write
 			//어떤 파일을 가져와야하는지 확인해서 그 파일을 보낼 수 있도록 한다.
@@ -210,9 +206,9 @@ void  ServManager::sockReadable(struct kevent *cur_event){
 			return ;
 		}
 		if (raw_data_ref.size() == 0){
-			cur_udata->http_response_.reserve(cur_udata->http_request_.size());
-			for(size_t i = 0; i < cur_udata->http_request_.size(); i++){
-				cur_udata->http_response_[i].makeResponse(cur_udata->http_request_[i]);
+			cur_udata->http_response_.reserve(http_request_ref.size());
+			for(size_t i = 0; i < http_request_ref.size(); i++){
+				cur_udata->http_response_[i].makeResponse(http_request_ref[i]);
 			}
 			Kqueue::registerWriteEvent(cur_event->ident, cur_event->udata);
 			Kqueue::unregisterReadEvent(cur_event->ident, cur_event->udata);//TODO: 나중에 Write Event가 끝나고 Udata delete 필요

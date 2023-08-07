86e757165dfa9330b7899a7722c310d378b993da
Junghyun Jin <jjin@c4r1s6.42seoul.kr>
2023-08-07 22:04:42 +0900

    Fix: UData 복원 예전꺼 복붙해왔음

--- modules/UData/UData.cpp
+++ modules/UData/UData.cpp
@@ -1,13 +1,4 @@
 #include "UData.hpp"
-
 UData::UData(e_fd_type type): status_code_(-1),client_fd_(-1),write_size_(0){ fd_type_ = type; }
 
 UData::~UData(){}
-
-void UData::generateResponses(void) {
-	for (size_t i = 0; i < http_request_.size(); i++) {
-		if (http_request_[i].getParseStatus() != FINISH)
-			break;
-		http_response_.push_back(HttpResponse(http_request_[i]));
-	}
-}
--- modules/UData/UData.hpp
+++ modules/UData/UData.hpp
@@ -5,31 +5,26 @@
 # include <vector>
 # include "./HttpRequest/HttpRequest.hpp"
 # include "./HttpResponse/HttpResponse.hpp"
-
 class HttpResponse;
 //OK가 HttpRequest file과 겹침
 // typedef enum errorType{
 // 	OK = 0
 // }e_error;
-
 typedef enum fdType{
 	CLNT,
 	CGI,
 	FILETYPE
 }e_fd_type;
-
 class UData{
 	public :
 		/* constructor */
-		UData();
+		UData(e_fd_type type);
 		~UData();
 
-		/* methods */
-		void		generateResponses(void);
-
 		/* variables */
+		e_fd_type					fd_type_;
 		std::vector<HttpRequest>	http_request_;
-		//HttpResponse	http_response_;
+		std::vector<HttpResponse>	http_response_;
 		// e_error 									error_flag_;
 		std::vector<char>					raw_data_;
 
@@ -37,7 +32,6 @@ class UData{
 		std::string				prog_name_;
 		std::vector<char>	cgi_store_;
 		pid_t							cgi_pid_;
-
 		/*임시 결과 저장소 for test*/
 		std::vector<char> ret_store_;//return 하는 값을 저장하는 저장소. 임시로 만듬
 		/*file*/
@@ -48,5 +42,4 @@ class UData{
 	private :
 		UData();
 };
-
 #endif

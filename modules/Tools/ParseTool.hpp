c2863cc177453873cef962d575ada9d8d3030e8f
wnddms12345 <wnddms12345@naver.com>
2023-08-07 22:32:10 +0900

    Fix: Http Method header change

--- modules/Tools/ParseTool.hpp
+++ modules/Tools/ParseTool.hpp
@@ -8,7 +8,7 @@
 # include <map>
 # include <algorithm>
 # include <cmath>
-
+# include "../UData/HttpMethod/HttpMethod.hpp"
 typedef enum blockType{
 	HTTPBLOCK,
 	SERVERBLOCK,
@@ -16,15 +16,6 @@ typedef enum blockType{
 	OTHERBLOCK
 }e_block;
 
-typedef enum methodType{
-	GET,
-	HEAD,
-	DELETE,
-	POST,
-	PUT,
-	PATCH,
-	OTHER_METHOD
-}e_method;
 //안하는 메서드들
 // OPTIONS, //서버가 지원하는 메서드를 판별할 수 있는 명령
 // COPY, //
